ef11a154879e4a2002b1d1578c446ac8acf95989
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-14 17:55:16 +0200

    Register Tag and Task models in Admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from app.models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "content", "is_done", "created_at", "deadline")
+    list_filter = ("is_done", "created_at", "deadline", "tags")
+    search_fields = ("content",)
+    filter_horizontal = ("tags",)
