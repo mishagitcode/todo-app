19d5b31c1138769a39d158078db1d05b5fd33376
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-14 18:17:21 +0200

    Implement CRUD in views.py for Task and Tag models

--- app/views.py
+++ app/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from app.forms import TaskForm, TagForm
+from app.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "todo/task_list.html"
+    context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:task-list")
+
+
+def toggle_task_status(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+    context_object_name = "tag_list"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo:tag-list")
